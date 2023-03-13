9b3628bc473bfc364ea7185c3c2e585e8a75dc86
Matthew Chau <26354088+TOLOTOLO999@users.noreply.github.com>
2023-03-12 19:41:17 -0700

    added comments and removed unused functions

--- preprocessing/session_based/preprocess_retailrocket.py
+++ preprocessing/session_based/preprocess_retailrocket.py
@@ -26,77 +26,6 @@ DAYS_TRAIN = 25
 DAYS_TEST = 2
 
 
-def preprocess_org(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_SESSION_LENGTH):
-    """
-    Preprocessing from original gru4rec
-    """
-    data, buys = load_data(path+file)
-    data = filter_data(data, min_item_support, min_session_length)
-    split_data_org(data, path_proc+file)
-
-
-def preprocess_org_min_date(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_SESSION_LENGTH, min_date=MIN_DATE):
-    """
-    Preprocessing from original gru4rec but from a certain point in time
-    """
-    data, buys = load_data(path+file)
-    data = filter_data(data, min_item_support, min_session_length)
-    data = filter_min_date(data, min_date)
-    split_data_org(data, path_proc+file)
-
-
-def preprocess_days_test(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_SESSION_LENGTH, days_test=DAYS_TEST):
-    """
-    Preprocessing adapted from original gru4rec
-    """
-    data, buys = load_data(path+file)
-    data = filter_data(data, min_item_support, min_session_length)
-    split_data(data, path_proc+file, days_test)
-
-
-def preprocess_days_test_min_date(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_SESSION_LENGTH, days_test=DAYS_TEST, min_date=MIN_DATE):
-    """
-    Preprocessing from original gru4rec but from a certain point in time
-    """
-    data, buys = load_data(path+file)
-    data = filter_data(data, min_item_support, min_session_length)
-    data = filter_min_date(data, min_date)
-    split_data(data, path_proc+file, days_test)
-
-
-def preprocess_slices(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_SESSION_LENGTH,
-                      num_slices=NUM_SLICES, days_offset=DAYS_OFFSET, days_shift=DAYS_SHIFT, days_train=DAYS_TRAIN, days_test=DAYS_TEST):
-    """
-    Preprocessing to create data slices with a window
-    """
-    data, buys = load_data(path+file)
-    data = filter_data(data, min_item_support, min_session_length)
-    slice_data(data, path_proc+file, num_slices, days_offset,
-               days_shift, days_train, days_test)
-
-
-def preprocess_info(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_SESSION_LENGTH):
-    """
-    Load and show info
-    """
-    data, buys = load_data(path+file)
-    data = filter_data(data, min_item_support, min_session_length)
-
-
-def preprocess_save(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_SESSION_LENGTH):
-    data, buys = load_data(path+file)
-    data = filter_data(data, min_item_support, min_session_length)
-    data.to_csv(path_proc + file + '_preprocessed.txt', sep='\t', index=False)
-
-
-def preprocess_buys(path=DATA_PATH, file=DATA_FILE, path_proc=DATA_PATH_PROCESSED):
-    """
-    Preprocessing to create a file with buy actions
-    """
-    data, buys = load_data(path+file)
-    store_buys(buys, path_proc+file)
-
-
 def load_data(file):
     # load csv
     data = pd.read_csv(file+'.csv', sep=',', header=0, usecols=[0, 1, 2, 3], dtype={
@@ -107,16 +36,21 @@ def load_data(file):
 
     # sessionize
     data.sort_values(by=['UserId', 'Time'], ascending=True, inplace=True)
+
     # compute the time difference between queries
     tdiff = np.diff(data['Time'].values)
+
     # check which of them are bigger then session_th
     split_session = tdiff > SESSION_LENGTH
     split_session = np.r_[True, split_session]
+
     # check when the user chenges is data
     new_user = data['UserId'].values[1:] != data['UserId'].values[:-1]
     new_user = np.r_[True, new_user]
+
     # a new sessions stars when at least one of the two conditions is verified
     new_session = np.logical_or(new_user, split_session)
+
     # compute the session ids
     session_ids = np.cumsum(new_session)
     data['SessionId'] = session_ids
@@ -161,85 +95,41 @@ def filter_data(data, min_item_support=MIN_ITEM_SUPPORT, min_session_length=MIN_
     return data
 
 
-def filter_min_date(data, min_date='2014-04-01'):
-    min_datetime = datetime.strptime(
-        min_date + ' 00:00:00', '%Y-%m-%d %H:%M:%S')
-
-    # filter
-    session_max_times = data.groupby('SessionId').Time.max()
-    session_keep = session_max_times[session_max_times >
-                                     min_datetime.timestamp()].index
-
-    data = data[np.in1d(data.SessionId, session_keep)]
-
-    # output
-    data_start = datetime.fromtimestamp(data.Time.min(), timezone.utc)
-    data_end = datetime.fromtimestamp(data.Time.max(), timezone.utc)
-
-    print('Filtered data set\n\tEvents: {}\n\tSessions: {}\n\tItems: {}\n\tSpan: {} / {}\n\n'.
-          format(len(data), data.SessionId.nunique(), data.ItemId.nunique(), data_start.date().isoformat(), data_end.date().isoformat()))
-
-    return data
-
-
-def split_data_org(data, output_file):
-    tmax = data.Time.max()
-    session_max_times = data.groupby('SessionId').Time.max()
-    session_train = session_max_times[session_max_times < tmax-86400].index
-    session_test = session_max_times[session_max_times >= tmax-86400].index
-    train = data[np.in1d(data.SessionId, session_train)]
-    test = data[np.in1d(data.SessionId, session_test)]
-    test = test[np.in1d(test.ItemId, train.ItemId)]
-    tslength = test.groupby('SessionId').size()
-    test = test[np.in1d(test.SessionId, tslength[tslength >= 2].index)]
-    print('Full train set\n\tEvents: {}\n\tSessions: {}\n\tItems: {}'.format(
-        len(train), train.SessionId.nunique(), train.ItemId.nunique()))
-    train.to_csv(output_file + '_train_full.txt', sep='\t', index=False)
-    print('Test set\n\tEvents: {}\n\tSessions: {}\n\tItems: {}'.format(
-        len(test), test.SessionId.nunique(), test.ItemId.nunique()))
-    test.to_csv(output_file + '_test.txt', sep='\t', index=False)
-
-    tmax = train.Time.max()
-    session_max_times = train.groupby('SessionId').Time.max()
-    session_train = session_max_times[session_max_times < tmax-86400].index
-    session_valid = session_max_times[session_max_times >= tmax-86400].index
-    train_tr = train[np.in1d(train.SessionId, session_train)]
-    valid = train[np.in1d(train.SessionId, session_valid)]
-    valid = valid[np.in1d(valid.ItemId, train_tr.ItemId)]
-    tslength = valid.groupby('SessionId').size()
-    valid = valid[np.in1d(valid.SessionId, tslength[tslength >= 2].index)]
-    print('Train set\n\tEvents: {}\n\tSessions: {}\n\tItems: {}'.format(
-        len(train_tr), train_tr.SessionId.nunique(), train_tr.ItemId.nunique()))
-    train_tr.to_csv(output_file + '_train_tr.txt', sep='\t', index=False)
-    print('Validation set\n\tEvents: {}\n\tSessions: {}\n\tItems: {}'.format(
-        len(valid), valid.SessionId.nunique(), valid.ItemId.nunique()))
-    valid.to_csv(output_file + '_train_valid.txt', sep='\t', index=False)
-
-
 def split_data(data, output_file, days_test):
+    """
+    Split the entire dataset into training and testing data
+    """
     data_end = datetime.fromtimestamp(data.Time.max(), timezone.utc)
     test_from = data_end - timedelta(days_test)
 
+    # Generate training and testing sessions
     session_max_times = data.groupby('SessionId').Time.max()
     session_train = session_max_times[session_max_times <
                                       test_from.timestamp()].index
     session_test = session_max_times[session_max_times >=
                                      test_from.timestamp()].index
+
+    # Generate training and testing sets
     train = data[np.in1d(data.SessionId, session_train)]
     test = data[np.in1d(data.SessionId, session_test)]
     test = test[np.in1d(test.ItemId, train.ItemId)]
+
+    # Filter out all single-event sessions
     tslength = test.groupby('SessionId').size()
     test = test[np.in1d(test.SessionId, tslength[tslength >= 2].index)]
+
+    # Output csv files
     print('Full train set\n\tEvents: {}\n\tSessions: {}\n\tItems: {}'.format(
         len(train), train.SessionId.nunique(), train.ItemId.nunique()))
     train.to_csv(output_file + '_train_full.txt', sep='\t', index=False)
+
     print('Test set\n\tEvents: {}\n\tSessions: {}\n\tItems: {}'.format(
         len(test), test.SessionId.nunique(), test.ItemId.nunique()))
     test.to_csv(output_file + '_test.txt', sep='\t', index=False)
 
 
 def slice_data(data, output_file, num_slices, days_offset, days_shift, days_train, days_test):
-
+    # For each slice of data, split to training and testing sets
     for slice_id in range(0, num_slices):
         split_data_slice(data, output_file, slice_id, days_offset +
                          (slice_id*days_shift), days_train, days_test)
@@ -275,6 +165,7 @@ def split_data_slice(data, output_file, slice_id, days_offset, days_train, days_
     sessions_test = session_max_times[session_max_times >=
                                       middle.timestamp()].index
 
+    # Training set
     train = data[np.in1d(data.SessionId, sessions_train)]
 
     print('Train set {}\n\tEvents: {}\n\tSessions: {}\n\tItems: {}\n\tSpan: {} / {}'.
@@ -283,9 +174,11 @@ def split_data_slice(data, output_file, slice_id, days_offset, days_train, days_
     train.to_csv(output_file + '_train_full.' +
                  str(slice_id)+'.txt', sep='\t', index=False)
 
+    # Testing set
     test = data[np.in1d(data.SessionId, sessions_test)]
     test = test[np.in1d(test.ItemId, train.ItemId)]
 
+    # Filter out single event sessions
     tslength = test.groupby('SessionId').size()
     test = test[np.in1d(test.SessionId, tslength[tslength >= 2].index)]
 
@@ -296,12 +189,5 @@ def split_data_slice(data, output_file, slice_id, days_offset, days_train, days_
                 '.txt', sep='\t', index=False)
 
 
-def store_buys(buys, target):
-    buys.to_csv(target + '_buys.txt', sep='\t', index=False)
-
-
-# -------------------------------------
-# MAIN TEST
-# --------------------------------------
 if __name__ == '__main__':
-    preprocess_slices()
+    pass
